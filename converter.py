47ee1fc3322acd93a2d5b5d482b5b5abc3f551d5
jonnyesports <j.villa@retouchmedia.com>
2025-04-14 16:51:26 -0500

    Added multiple options for converter

--- converter.py
+++ converter.py
@@ -1,10 +1,9 @@
 import PySimpleGUI as sg
 
 layout = [
-    [sg.Text("Convert KM into Miles", enable_events = True, key = "-TXT-")],
-    [sg.Input(key = "-INPUT-")],
+    [sg.Input(key = "-INPUT-"), sg.Spin(["km to mile", "kg to pound", "sec to min"], key = "-UNITS-")],
     [sg.Button("Submit", key = "-BUTT1-")],
-    [sg.Text("", key = "-MESSAGE-")]
+    [sg.Text("Hello!", key = "-MESSAGE-")]
 ]
 
 window = sg.Window("Converter", layout)
@@ -18,9 +17,16 @@ while True:
     if event == "-BUTT1-":
         input_value = values["-INPUT-"]
         if input_value.isnumeric():
-            km_conversion_to_miles = float(input_value) * 0.6214
-            window["-TXT-"].update(f"{input_value} kilometres is {km_conversion_to_miles} miles")
-            window["-MESSAGE-"].update("")
+            match values["-UNITS-"]:
+                case "km to mile":
+                    output = round(float(input_value) * 0.6214, 2)
+                    window["-MESSAGE-"].update(f"{input_value} kilometres is {output} miles")
+                case "kg to pound":
+                    output = round(float(input_value) * 2.20462, 2)
+                    window["-MESSAGE-"].update(f"{input_value} kg are {output} pounds")
+                case "sec to min":
+                    output = round(float(input_value) / 60, 2)
+                    window["-MESSAGE-"].update(f"{input_value} seconds are {output} minutes")
         else:
             window["-MESSAGE-"].update("You did not enter a valid number")
 
